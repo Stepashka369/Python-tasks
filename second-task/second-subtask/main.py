e983814b7939c2a9e973838594cd49d57456b6eb
Stepa <kardash-99@list.ru>
2024-06-14 15:55:02 +0300

    1 Ready but without tests

--- second-task/second-subtask/main.py
+++ second-task/second-subtask/main.py
@@ -1,8 +1,22 @@
 import utility_iperf
+import argparse
+
+def parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('ip_server') #ip сервера
+    parser.add_argument('hostname_server') #имя учетки сервера
+    parser.add_argument('password_server') #пароль сервера
+    parser.add_argument('ip_client') #ip клиента
+    parser.add_argument('hostname_client') #имя учетки клиента
+    parser.add_argument('password_client') #пароль клиента
+    return parser.parse_args()
 
 
 def main():
-    util = utility_iperf.Utility('10.0.2.3', 'weber', 'weber', '10.0.2.2', 'weber', 'weber')
+    # util = utility_iperf.Utility('10.0.2.3', 'weber', 'weber', '10.0.2.2', 'weber', 'weber')
+    args = parser()
+    util = utility_iperf.Utility(args.ip_server, args.hostname_server, args.password_server, 
+                                 args.ip_client, args.hostname_client, args.password_client)
     util.run()
    
 
--- second-task/second-subtask/utility_iperf.py
+++ second-task/second-subtask/utility_iperf.py
@@ -1,9 +1,12 @@
 import utility_ssh
 import threading
 import time
+import json
 from typing import Tuple
 
 class Utility:
+    TIMEOUT = 15
+
     def __init__(self, server_ip: str, server_username: str, server_password: str,
                  client_ip: str, client_username: str, client_password: str):
         self.server_ip = server_ip
@@ -27,7 +30,7 @@ class Utility:
         self.server = self._run_remote_command(self.server_ip, self.server_username, self.server_password, command)
 
     
-    def _stop_process(self, ip_remote: str, username_remote: str, password_remote: str, pid: int):
+    def _stop_process(self, ip_remote: str, username_remote: str, password_remote: str, pid: int) -> None:
         command = f'kill {pid}'
         self._run_remote_command(ip_remote, username_remote, password_remote, command)
    
@@ -61,26 +64,42 @@ class Utility:
             job_time = time.time() - start_time
 
         return False, pid
+    
+
+    def _parse_result(self) -> None:
+        result = { 'error' : self.client.stderr,
+                  'result' : json.loads(self.client.stdout),
+                  'status' : self.client.returncode}
+        self.result = json.dumps(result, indent=2)
+       
+
+    def print_result(self) -> None:
+        print(self.result)
 
 
-    def run(self):
+    def run(self) -> None:
         print('Starting server.')
         server_thread = threading.Thread(target=self._run_server)
         server_thread.start()
-        server_running, server_pid = self._is_running(self.server_ip, self.server_username, self.server_password, 15)
+        server_running, server_pid = self._is_running(self.server_ip, self.server_username, self.server_password, self.TIMEOUT)
 
         if server_running:
             print('Starting client.')
             client_thread = threading.Thread(target=self._run_client)
             client_thread.start()
-            client_running, client_pid = self._is_running(self.client_ip, self.client_username, self.client_password, 15)
+            client_running, client_pid = self._is_running(self.client_ip, self.client_username, self.client_password, self.TIMEOUT)
             if client_running:
-                client_thread.join(15)
+                client_thread.join(self.TIMEOUT)
                 self._stop_process(self.client_ip, self.client_username, self.client_password, client_pid)
                 self._stop_process(self.server_ip, self.server_username, self.server_password, server_pid)
-                self.client.print_result()
+                server_thread.join()
             else:
+                self._stop_process(self.server_ip, self.server_username, self.server_password, server_pid)
                 print('Client startup error.')
         else:
             print('Server startup error.')
+
+        self._parse_result()
+        self.print_result()
+        # self.client.print_result()
         
